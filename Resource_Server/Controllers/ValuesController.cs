765423c756b4bac056302cce25054660d8a12507
Pete Evans <pevans@marathonsystems.com.au>
2018-11-04 12:01:09 +1100

    Added Authorization Policy Enforcement on HTTP GET/PUT/DELETE

--- Resource_Server/Controllers/ValuesController.cs
+++ Resource_Server/Controllers/ValuesController.cs
@@ -3,41 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 namespace Resource_Server.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
         // GET api/values
         [HttpGet]
+        [Route("api/intersite/premiumcontent")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "IncubatorAuthZ")]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
 
         // POST api/values
         [HttpPost]
+        [Route("api/intersite/premiumcontent")]
+
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
+        [Route("api/intersite/premiumcontent")]
+
         public void Delete(int id)
         {
         }
