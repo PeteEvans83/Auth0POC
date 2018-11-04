e4990146a2b2a5b4626ced64f4b35ae91ff604bf
Pete Evans <pevans@marathonsystems.com.au>
2018-11-04 11:59:24 +1100

    Added backend API call to App
    Added Axios dependency
    Injected bearer access token from local storage
    Hit backend API with CORS allows headers.

--- application/src/App.js
+++ application/src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import { Navbar, Button } from "react-bootstrap";
+import axios from 'axios';
+
 import "./App.css";
 
 class App extends Component {
+
+  state = {
+    message: ""
+  }
+
+
+
   goTo(route) {
     this.props.history.replace(`/${route}`);
   }
@@ -14,6 +23,23 @@ class App extends Component {
   logout() {
     this.props.auth.logout();
   }
+
+  readData() {
+    var accessToken =  localStorage.getItem('access_token');
+
+
+
+
+var config = {
+  headers: { 'Access-Control-Allow-Origin': '*','Authorization': `Bearer ${accessToken}` }
+}
+
+    
+    axios.get('http://localhost:5000/api/intersite/premiumcontent',  config )
+      .then(response => this.setState({ message: response.data.message }))
+      .catch(error => this.setState({ message: error.message }));
+  }
+
 
   render() {
     const { isAuthenticated } = this.props.auth;
@@ -51,10 +77,35 @@ class App extends Component {
               >
                 Log Out
               </Button>
+            
             )}
+            {isAuthenticated() && (
+              <Button
+                id="qsLogoutBtn"
+                bsStyle="primary"
+                className="btn-margin"
+                onClick={this.readData.bind(this)}
+              >
+                Get Data
+              </Button>
+            
+            )}
+
+
+  
+
+
+
+
+
+
           </Navbar.Header>
         </Navbar>
       </div>
+
+
+
+
     );
   }
 }
