55ea7d9c6dcfbfa8ac954c264e56e6fea9dbe460
Peter Evans <pevans@marathonsystems.com.au>
2018-11-04 15:35:16 +1100

    merged.

--- Resource_Server/Startup.cs
+++ Resource_Server/Startup.cs
@@ -4,39 +4,76 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+
 
 namespace Resource_Server
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddAuthentication(options =>
+               {
+                   options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                   options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+               }).AddJwtBearer(options =>
+               {
+                   options.Authority = "https://petereevansauth0.au.auth0.com";
+                   options.Audience = "http://not-piedpiper.com.au/api/intersite/premiumcontent";
+                   options.TokenValidationParameters.ValidateLifetime = true;
+                   options.TokenValidationParameters.ClockSkew = TimeSpan.Zero;
+               });
+
+            services.AddAuthorization(options =>
+             {
+
+
+                 options.AddPolicy("IncubatorAuthZ", policy => policy.RequireClaim("http://not-piedpiper.com.au/role", "Incubator"));
+                 options.AddPolicy("CEOAuthZ", policy => policy.RequireClaim("http://not-piedpiper.com.au/role", "CEO"));
+                 options.AddPolicy("InvestorAuthZ", policy => policy.RequireClaim("http://not-piedpiper.com.au/role", "Investor"));
+             });
+
+            services.AddCors();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
 
+            app.UseCors(builder => builder.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod());
+
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+//            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
